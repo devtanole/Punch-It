588837e460cb9f44b38d9242450b91921eb56620
Devin Olesen <devintanielu@gmail.com>
2025-04-02 01:59:20 +0000

    setup repo/deploy

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,4 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
-
--- EXAMPLE:
-
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+insert into "posts" ("textContent", "mediaUrls")
+  values
+  ('this is the first Punch It post, everyone else is late to the party', 'https://churchonthesound.org/wp-content/uploads/2020/03/This-is-a-test.jpg'),
+  ('dang i guess i was late, this is the second sample post', 'https://media.istockphoto.com/id/491520707/photo/sample-red-grunge-round-stamp-on-white-background.jpg?s=612x612&w=0&k=20&c=FW80kR5ilPkiJtXZEauGTghNBOgQviVPxAbhLWwnKZk=')
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,47 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+  "userId" serial PRIMARY KEY,
+  "email" varchar UNIQUE NOT NULL,
+  "fullName" text NOT NULL,
+  "bio" text NOT NULL,
+  "username" text UNIQUE NOT NULL,
+  "hashedPassword" text NOT NULL,
+  "userType" varchar NOT NULL,
+  "createdAt" timestamptz NOT NULL,
+  "location" text NOT NULL
+);
+
+CREATE TABLE "fighter_profile" (
+  "userId" int PRIMARY KEY,
+  "height" text,
+  "weight" int,
+  "record" text,
+  "gymName" text,
+  "pullouts" int,
+  "weightMisses" int,
+  "finishes" int
+);
+
+CREATE TABLE "promoter_profile" (
+  "userId" int PRIMARY KEY,
+  "promotion" text NOT NULL,
+  "promoter" text NOT NULL,
+  "nextEvent" timestamptz
+);
+
+CREATE TABLE "posts" (
+  "postId" serial PRIMARY KEY,
+  "userId" int,
+  "textContent" text NOT NULL,
+  "mediaUrls" text[],
+  "createdAt" timestamptz NOT NULL DEFAULT 'now()'
+);
+
+ALTER TABLE "fighter_profile" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "promoter_profile" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
+
+ALTER TABLE "posts" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
