a2a13c5481cf8a4d96f7003b922cd67b0e8708a8
Devin Olesen <devintanielu@gmail.com>
2025-04-27 02:06:09 +0000

    clean up header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,50 +1,68 @@
 import { Outlet, Link, useNavigate } from 'react-router-dom';
 import { useUser } from './useUser';
-
-//later on add conditional render for logout button
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  Box,
+} from '@mui/material';
 
 export function Header() {
   const { user, handleSignOut } = useUser();
   const navigate = useNavigate();
+
   function handleClick(): void {
     handleSignOut();
     navigate('/');
   }
   return (
     <>
-      <header className="border-b border-gray-200 mt-0 w-full">
-        <nav className="bg-white w-full">
-          <ul
-            className=" flex items-center justify-center w-full "
-            style={{ listStyle: 'none' }}>
-            <li className="inline-block py-2 px-0">
-              <Link to="/">
-                <img
-                  src="/images/gloveLogo.png"
-                  alt="punch it boxing glove logo"
-                  className="h-20 w-auto "
-                />
-              </Link>
-            </li>
-            <li className="flex-1 text-center py-2">
-              <span
-                className="text-5xl font-bold"
-                style={{ fontFamily: 'Bangers' }}>
-                Punch It
-              </span>
-            </li>
-          </ul>
-          {user ? (
-            <button onClick={handleClick} className="entries-link white-text">
-              <h3>Sign Out</h3>
-            </button>
-          ) : (
-            <Link to="/auth/sign-in" className="entries-link white-text">
-              <h3>Sign In</h3>
+      <AppBar position="sticky" sx={{ backgroundColor: 'white' }}>
+        <Toolbar>
+          <Container sx={{ display: 'flex', alignItems: 'center' }}>
+            <Link
+              to="/"
+              style={{
+                textDecoration: 'none',
+                display: 'flex',
+                alignItems: 'center',
+              }}>
+              <img
+                src="/images/gloveLogo.png"
+                alt="punch it boxing glove logo"
+                style={{ height: 50, width: 'auto', marginRight: 10 }}
+              />
             </Link>
-          )}
-        </nav>
-      </header>
+
+            <Box
+              sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+              <Typography
+                variant="h4"
+                sx={{
+                  fontFamily: 'Bangers',
+                  color: 'black',
+                  textAlign: 'center',
+                }}>
+                Punch It
+              </Typography>
+            </Box>
+            <div>
+              {user ? (
+                <Button color="primary" onClick={handleClick}>
+                  Logout
+                </Button>
+              ) : (
+                <Link to="/auth/sign-in" style={{ textDecoration: 'none' }}>
+                  <Button color="primary">Login</Button>
+                </Link>
+              )}
+            </div>
+          </Container>
+        </Toolbar>
+      </AppBar>
+
       <Outlet />
     </>
   );
