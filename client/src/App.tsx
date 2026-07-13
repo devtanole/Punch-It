c632f128168dddaab7c9b91def2b4705db23a0f8
Devin Olesen <devintanielu@gmail.com>
2026-07-13 20:31:27 +0000

    protect routes from front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,7 @@ import { FightHistoryForm } from './pages/FightHistoryForm';
 import { MessagesInbox } from './pages/MessagesInbox';
 import { ConversationThread } from './pages/ConversationThread';
 import { LandingPage } from './pages/LandingPage';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 export default function App() {
   return (
@@ -21,16 +22,21 @@ export default function App() {
           <Route index element={<LandingPage />} />
           <Route path="/auth/sign-up" element={<AuthPage mode="sign-up" />} />
           <Route path="/auth/sign-in" element={<AuthPage mode="sign-in" />} />
-          <Route path="/feed" element={<PostFeed />} />
-          <Route path="details/:postId" element={<PostForm />} />
-          <Route path="/fights/:fightId" element={<FightHistoryForm />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-          <Route path="/messages" element={<MessagesInbox />} />
-          <Route
-            path="/messages/:conversationId"
-            element={<ConversationThread />}
-          />
-          <Route path="/search" element={<UserSearchBar />} />
+
+          {/* Protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/feed" element={<PostFeed />} />
+            <Route path="details/:postId" element={<PostForm />} />
+            <Route path="/fights/:fightId" element={<FightHistoryForm />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/messages" element={<MessagesInbox />} />
+            <Route
+              path="/messages/:conversationId"
+              element={<ConversationThread />}
+            />
+            <Route path="/search" element={<UserSearchBar />} />
+          </Route>
+
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
